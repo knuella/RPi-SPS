2f013042acb75f037f89fd82d30442414e8efe30
Felix Dietrich <felix.dietrich@sperrhaken.name>
2014-05-31 21:51:18 +0200

    Fix stuff in controller.py

--- src/controller.py
+++ src/controller.py
@@ -1,6 +1,7 @@
 import zmq
 
 
+message_list = []
 
 def is_valid_publisher_message(message):
     """
@@ -12,14 +13,15 @@ def is_valid_publisher_message(message):
     return True
 
 
-def recv_new_values(source, values_push):
+def recv_new_values(source):
     m = source.recv_multipart()
+    print(m)
     if is_valid_publisher_message(m):
-        message_list.append()
+        message_list.append(m)
     # silently drop invalid messages for now
 
 
-def publish_new_values(publisher, values_pull):
+def publish_new_values(publisher):
     try:
         m = message_list.pop()
     except IndexError:
@@ -33,16 +35,16 @@ def main():
     pub = context.socket(zmq.PUB)
     sources_pull = context.socket(zmq.PULL)
 
-    pub.bind("tcp://localhost:5555")
-    sources_pull.bind("tcp://localhost:5556")
+    pub.bind("tcp://127.0.0.10:5555")
+    sources_pull.bind("tcp://127.0.0.10:5556")
 
     poller = zmq.Poller()
-    poller.register(sources_pull)
+    poller.register(sources_pull, flags=zmq.POLLIN)
 
     # main loop
     # should use some library for event-driven development
     while True:
-        if poller.poll():
+        if len(poller.poll(timeout=0)) > 0:
             recv_new_values(sources_pull)
 
         publish_new_values(pub)
